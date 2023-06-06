3740ad164f2f9357419f43f4578e3c7c12b9dd97
dev-library <dev.rary@gmail.com>
2023-06-07 07:54:36 +0900

    단축URL DDD 적용

---
+++ src/main/java/kr/co/shortenurlservice/presentation/ShortenUrlCreateRequestDto.java
@@ -0,0 +1,22 @@
+package kr.co.shortenurlservice.presentation;
+
+import org.hibernate.validator.constraints.URL;
+
+import javax.validation.constraints.NotNull;
+
+public class ShortenUrlCreateRequestDto {
+    @NotNull
+    @URL(regexp = "[(http(s)?):\\/\\/(www\\.)?a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)")
+    private String originalUrl;
+
+    public ShortenUrlCreateRequestDto() {
+    }
+
+    public ShortenUrlCreateRequestDto(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+}
